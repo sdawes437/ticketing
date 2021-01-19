655b71b373de5da3cc791de75f398a320b806be4
sdawes437 <sdawes437@gmail.com>
2021-01-19 22:19:20 +0000

    Added user model, password hashing logic

--- auth/src/errors/request-validation-errors.ts
+++ auth/src/errors/request-validation-errors.ts
@@ -4,7 +4,7 @@ import { CustomError } from './custom-errors';
 export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Invalid request parameters');
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
