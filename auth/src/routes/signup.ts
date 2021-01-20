42d9ebf285e7827d67f65a8daab044108759c382
sdawes437 <sdawes437@sky.com>
2021-01-20 16:32:09 +0000

    jwt and cookie setup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,19 +1,21 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidationError } from '../errors/request-validation-errors';
-import { User } from '../models/user';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { BadRequestError } from "../errors/bad-request-error";
+import { RequestValidationError } from "../errors/request-validation-errors";
+import { User } from "../models/user";
 
 const router = express.Router();
 
 router.post(
-  '/api/users/signup',
+  "/api/users/signup",
   [
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password')
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters'),
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -27,11 +29,25 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError('Email in use');
+      throw new BadRequestError("Email in use");
     }
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "asdf"
+    );
+
+    // Store on session object
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
