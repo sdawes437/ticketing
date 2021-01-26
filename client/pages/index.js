dd8da330dfefdb4fb7738fde310e13b41aeb3f88
sdawes437 <sdawes437@gmail.com>
2021-01-26 19:38:19 +0000

    Client auth completed

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,19 @@
-export default () => {
-  return <h1>Landing Page</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  console.log('LANDING PAGE!');
+  return data;
+};
+
+export default LandingPage;
