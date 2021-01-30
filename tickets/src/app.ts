2276757d2dfa51adf62be67ee9f9b431c43f43aa
sdawes437 <sdawes437@gmail.com>
2021-01-30 22:07:11 +0000

    Added tests and routes for show and index routes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -9,6 +9,8 @@ import {
   currentUser,
 } from '@sdawesdevelopment/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -21,6 +23,8 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
