5fb3ef2caf2324d2767c73da1a3a2d967865f20c
sdawes437 <sdawes437@gmail.com>
2021-02-18 14:12:58 +0000

    Completed new order route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,60 @@
 import express, { Request, Response } from 'express';
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@sdawesdevelopment/common';
+import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [body('ticketId').not().isEmpty().withMessage('TicketId must be provided')],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    // Find the ticket the user is trying to order in the database
+
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure this ticket is not already reserved
+
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the database
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket: ticket,
+    });
+
+    await order.save();
+    // Publish an event saying that an order was created
+    res.status(201).send(order);
+  }
+);
 
 export { router as newOrderRouter };
