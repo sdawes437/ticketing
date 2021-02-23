da404380d96592d2b5b29559a3f06b8e185f3d75
sdawes437 <sdawes437@gmail.com>
2021-02-23 11:03:19 +0000

    Added event publishing

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,6 +5,9 @@ import {
   requireAuth,
 } from '@sdawesdevelopment/common';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -14,7 +17,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -27,6 +30,13 @@ router.delete(
     order.status = OrderStatus.Cancelled;
 
     await order.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
