218f740718d04afd2a6b7a4b542c0abb3ceaf63e
Sam Dawes <sdawes437@gmail.com>
2021-03-08 17:57:53 +0000

    Added expiration service

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,22 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  Subjects,
+} from "@sdawesdevelopment/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject: Subjects.OrderCreated = Subjects.OrderCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
+  }
+}
