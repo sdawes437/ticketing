fdbbe0e65e34954a0dd2b5d43af893c3c78725ac
Sam Dawes <sdawes437@gmail.com>
2021-03-15 21:44:23 +0000

    Building client

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        currency="GBP"
+        stripeKey="pk_test_51IUDu0BN7ioqe2glyPiql50EeBv4KY7lrMR1YUoji9ICJcBy62EQcDmGTU8xnPnihvC4salT4ofG2NpSMx6H21Qw00p3PveTWk"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+    </div>
+  );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -12,6 +12,8 @@ import { Order } from '../models/order';
 import { Payment } from '../models/payment';
 
 import { stripe } from '../stripe';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -53,7 +55,13 @@ router.post(
 
     await payment.save();
 
-    res.status(201).send({ success: true });
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
+    res.status(201).send({ id: payment.id });
   }
 );
 
