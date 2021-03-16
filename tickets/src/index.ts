9039cf70be3aa56973e71341d8a0d172c488a9eb
Sam Dawes <sdawes437@gmail.com>
2021-03-16 16:39:40 +0000

    Minor changes

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,28 +1,29 @@
-import mongoose from "mongoose";
-import { app } from "./app";
-import { natsWrapper } from "./nats-wrapper";
-import { OrderCreatedListener } from "./events/listeners/order-created-listener";
-import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
+import mongoose from 'mongoose';
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
+  console.log('Starting up...');
   if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
+    throw new Error('JWT_KEY must be defined');
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URL must be defined");
+    throw new Error('MONGO_URL must be defined');
   }
 
   if (!process.env.NATS_CLIENT_ID) {
-    throw new Error("NATS_CLIENT_ID must be defined");
+    throw new Error('NATS_CLIENT_ID must be defined');
   }
 
   if (!process.env.NATS_URL) {
-    throw new Error("NATS_URL must be defined");
+    throw new Error('NATS_URL must be defined');
   }
 
   if (!process.env.NATS_CLUSTER_ID) {
-    throw new Error("NATS_CLUSTER_ID must be defined");
+    throw new Error('NATS_CLUSTER_ID must be defined');
   }
 
   try {
@@ -32,13 +33,13 @@ const start = async () => {
       process.env.NATS_URL
     );
 
-    natsWrapper.client.on("close", () => {
-      console.log("NATS connection closed!");
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
       process.exit();
     });
 
-    process.on("SIGINT", () => natsWrapper.client.close());
-    process.on("SIGTERM", () => natsWrapper.client.close());
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
 
     new OrderCreatedListener(natsWrapper.client).listen();
     new OrderCancelledListener(natsWrapper.client).listen();
@@ -48,13 +49,13 @@ const start = async () => {
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000");
+    console.log('Listening on port 3000');
   });
 };
 
